e932178f4cca6edffb66b8e883f59e35a4f7cd9b
Greg261090 <carlosgregorio13@yahoo.com.br>
2025-12-09 11:00:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,38 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', e => {});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('meu-dinheiro-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-256.png',
+        '/icons/icon-512.png'
+      ]);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+
+// Notificações Push (placeholder)
+self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : {
+    title: 'Meu Dinheiro',
+    body: 'Notificação recebida.'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icons/icon-256.png'
+    })
+  );
+});
